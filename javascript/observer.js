efe60e48e82073c0a80346e6d520d73ee5b69bcb
knavesmireBoy <andrewsykes@btinternet.com>
2017-06-21 15:39:51 +0100

    removed underscores

--- javascript/observer.js
+++ javascript/observer.js
@@ -1,70 +1,114 @@
-var gAlp = gAlp || {};
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+if (!window.gAlp) {
+    window.gAlp = {};
+}
 
+gAlp.Publish = (function () {
+    "use strict";
+    
+    var slice = Array.prototype.slice;
 
-gAlp.Observer = {
+    function getfunction(fn, cn) {
+        var context = typeof cn === "undefined" ? null : cn || this,
+            func = typeof fn === 'function' ? fn : context ? context[fn] : undefined;
+        return func;
+    }
+    
+    function visitSubscribers(action, type, arg, context) {
+                    
 
-    subscribers: {any: []/*, special: []*/}, // event type: subscribers
-
-    on: function (type, fn, context) {
-
-        var pushIf = function (h, n) {
-                h = h.filter(function (el) {
-                    return el.fn !== n;
-                });
-                h.push({fn: n, context: context, type: type});
-                subs[type] = h;//**1 h is A NEW array reASSIGN to subscribers (subs available to this function)
-            },
-            subs = this.subscribers;
-
-
-        type = type || 'any';
-        context = typeof context === "undefined" ? null : context || this;
-        fn = typeof fn === 'function' ? fn : context ? context[fn] : undefined;
-
-        //this line is what allows the attempt to Observe unavailable subscribers
-        if (typeof fn === "undefined") {
-            return this;
-        }
-        if (typeof subs[type] === "undefined") {
-            subs[type] = [];
-        }
-        pushIf(subs[type], fn);
-        return this;
-    },
-
-
-        remove: function (type, fn, context) {
-    context = context || this;
-    this.visitSubscribers('unsubscribe', type, fn, context);
-    return this;
-},
-
-fire: function (type, data, bool) {
-    this.visitSubscribers('Publish', type, data, null, bool);
-    return this;
-},
-
-visitSubscribers: function (action, type, arg, context, bool) {
-
-    var pubtype = type || 'any',
-        subscribers = this.subscribers[pubtype],
-        i, current,
+            //signature for remove remains the same ('arg' === function)
+        //for publish 'arg' could be multiple arguments and context isn't required
+        var pubtype = type || 'any',
+            subscribers,
+            i,
+            current,
+            max,
+            args = slice.call(arguments, 2);//see above
+        
+        
+        subscribers = this.subscribers[pubtype];
         max = subscribers ? subscribers.length : 0;
+        
+        for (i = 0; i < max; i += 1) {
+            current = subscribers[i];
+            if (action === 'publish') {
 
-    for (i = 0; i < max; i += 1) {
-        current = subscribers[i];
-        if (action === 'Publish') {
-            current.fn.call(current.context, arg, current.type, bool);
+                //current.func.call(current.context, arg);
+                current.func.apply(current.context, args);
+               // current.context[current.func](arg);
 
-        } else {
-            if (current.fn === arg && current.context === context) {
-                subscribers.splice(i, 1);
+            } else {
+                if (current.func === arg && current.context === context) {
+                    subscribers.splice(i, 1);
+                }
             }
         }
     }
-}
 
+    return {
 
-};//gAlp.Observer
+        subscribers: {
+            set: function (prop, arg) {
+                this[prop] = arg || [];
+            },
 
+            get: function (prop) {
+                return this[prop];
+            },
 
+            any: []
+        },
+        
+        
+        /*NOTE if fn is a string then storing string and context as distinct values and invoking as context[fn]()
+        allows for changes to the function referenced ie lazy function definition.. Otherwise the original function is effectively a closure*/
+        on: function (Type, fn, context) {
+            var subs = this.subscribers,
+                type = Type || 'any',
+                func = getfunction.call(this, fn, context);
+            
+              //  func = fn;
+
+            if (typeof func === "undefined") { return; }
+            if (typeof subs[type] === "undefined") { subs[type] = []; }
+
+           subs[type] = subs[type].filter(function (member) {
+                return member.func !== func;
+            });
+
+            subs[type].push({
+                func: func,
+                context: context
+            });
+            
+        },
+
+        remove: function (type, func, context) {
+            context = context || this;
+            visitSubscribers.call(this, 'unsubscribe', type, func, context);
+        },
+
+        fire: function (type, data) {
+            var args = slice.call(arguments, 1);
+           // return visitSubscribers.call(this, 'publish', type, data);
+            return visitSubscribers.apply(this, ['publish', type].concat(args));
+        },
+
+/*
+        publish: function (object) {
+            var prop;
+            //effectively init BUT we're mixing in this properties to incoming object which should possess an init method
+            if (object && object.subscribers) {
+                return;
+            }
+            for (prop in this) {
+                if (this.hasOwnProperty(prop)) {
+                    object[prop] = this[prop];
+                }
+            }
+        }*/
+    };//returned
+
+}()); //gAlp.Publish
--- javascript/tagBuilder.js
+++ javascript/tagBuilder.js
@@ -4,6 +4,7 @@ if (!window.gAlp) {
     window.gAlp = {};
 }
 
+
 window.gAlp.tagBuilder = (function () {
     "use strict";
 
@@ -11,15 +12,40 @@ window.gAlp.tagBuilder = (function () {
         return tag.split("_");
     }
 
-    function makeTag(open, tag, close) {
-        return open + tag + close || "";
+    function makeTag(tag) {
+        return document.createElement(tag);
+    }
+    
+    function appendIt(anchor, el, flag){
+                if(!flag) return anchor.appendChild(el);
+       return anchor.insertBefore(el, anchor.firstChild);
+        //return anchor.prependChild(el);
+    }
+    
+    function setAttrs(data){
+        for(var prop in data){
+            this.setAttribute(prop, data[prop]);
+        }
+        return this;
+    }
+    
+    function setContent(data){
+       this.innerHTML = data;
+        return this;
     }
 
     function makeJQ(arg) {
-        if (arg instanceof jQuery) {
+        if (typeof jQuery !== "undefined" && arg instanceof jQuery) {
             return arg;
         }
         return $(arg);
+    }
+    
+     function notJQ(arg) {
+        if (typeof jQuery !== "undefined" && arg instanceof jQuery) {
+            return arg[0];
+        }
+        return arg;
     }
 
     function flush(collection) {
@@ -27,8 +53,8 @@ window.gAlp.tagBuilder = (function () {
             c = collection,
             L = c.length;
         for (i; i < L; i++) {
-            if (i) {
-                c[0] = c[0].wrap(c[i]);
+            if (i) {//exclude first
+                c[0] = c[0].wrap(c[i]);//recursivley decorating the initial method
             }
         }
         c.splice(1);
@@ -37,63 +63,67 @@ window.gAlp.tagBuilder = (function () {
 
     return {
 
-        build: function (data, $anchor) {
-            
-            this.$anchor = makeJQ($anchor);
-
+        build: function (data, anchor) {
+                        
+            //this.$anchor = makeJQ($anchor);
+            this.anchor = notJQ(anchor);
             var that = this,
                 gang,
                 prop,
                 arg,
                 method,
-                anchor = function (sibling) {
-                    if (sibling) {
-                        return that.$anchor.parent();
+                getAnchor = function (sibling) {
+                    if (parseFloat(sibling)) {
+                        return that.anchor.parentNode;
                     }
-                    return that.$anchor;
+                    return that.anchor;
                 },
 
                 process = {
                     tag: function (tag) {
-                        return function (fn) {
+                          return function (fn) {
                             /*see config.contents.placeholder.children
                             We need to determine whether to append to current or parent tag, "_sibling" added to config: tag to indicate status. String.split returns an array at "_", if array has length of 2 we are dealing with a sibling tag and must reset current anchor to common parent*/
-                            var tg = isSibling(tag);
-                            return $(makeTag("<", tg[0], "/>")).appendTo(fn(tg[1]));
+                            var tg = isSibling(tag);//anchor function will be called to determine the dom element to anchor to
+                           //tg[1] will be a string or undefined
+                            return appendIt(fn(tg[1]), makeTag(tg[0]), tg[2]);
                         };
                     },
                     attrs: function (attrs) {
                         return function (fn) {
-                            return fn().attr(attrs);
+                            //return fn().attr(attrs);
+                            return setAttrs.call(fn(), attrs);
                         };
                     },
                     content: function (content) {
                         var that = this;
                         return function (fn) {
-                            return fn().html(content);
+                            //return fn().html(content);
+                            return setContent.call(fn(), content);
                         };
                     },
                     children: function (kids) {
                         var i = 0;
-                        that.$anchor = flush(gang);
+                        that.anchor = flush(gang);
                         while (kids[i]) {
-                            that.build(kids[i], that.$anchor);
+                            that.build(kids[i], that.anchor);
                             i++;
                         }
                     }
                 };
 
-            gang = [anchor];
+            gang = [getAnchor];
 
             for (prop in data) {
-                //if (data.hasOwnProperty(prop)) {
+              if (data.hasOwnProperty(prop) || Object.getPrototypeOf(data).hasOwnProperty(prop)) {
                     method = process[prop].bind(this);
                     arg = data[prop];
                     gang.push(method(arg));
-                //}
+               }
             }
-            //elements with no children 
-            if (gang[1]) {
+           //ie if more than initial method 'getAnchor'
+            if (gang[1]) {//children method flushes data and sets new anchor
+                //elements with no children get flushed here
                 flush(gang);
             }
 
