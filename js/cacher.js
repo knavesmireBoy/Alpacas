05dd052b1052f8eefcb8da514eaa6b3d525f13c7
Andrew Sykes <andrewsykes@btinternet.com>
2022-07-26 12:15:17 +0100

    console

--- js/cacher.js
+++ js/cacher.js
@@ -78,19 +78,24 @@ gAlp.Cacher = function(path, lo, hi) {
     swap(document.getElementsByTagName('a'), filter('href'), doCallback('href'));
     
      if(window.localStorage) {
+         console.log('stor')
          if(!window.localStorage.cachedElements) {
              window.localStorage.cachedElements = "";
          }
         if(!matched) return;
+         console.log('match')
 		if (isCached(matched)) {
+            console.log('ismatch')
 			doSwap();
             //using this to speed up fade from grey when images cached
             document.documentElement.classList.add('done');
 		} else {
+            console.log('cache')
 			swap(images, filter('src'), doCache);
 		}
      }
     else {
+        console.log('nostor')
         doSwap();
     }
 };
